b065ba8adb8728fc323c94159c5db1ac2b303bdc
Novi4ek2156 <vlad_semyon@bk.ru>
2026-03-03 23:24:52 +0700

    changes

--- app/src/main/java/com/example/izbushka/MainActivity.kt
+++ app/src/main/java/com/example/izbushka/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.izbushka
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, InfoMenu::class.java)
+            startActivity(intent)
         }
+    }
+}
+
+class InfoMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.info_menu)
     }
 }
