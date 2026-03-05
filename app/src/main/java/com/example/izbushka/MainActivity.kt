0eb3d058eb1be149f50a266e12e89ebcee4e8910
Novi4ek2156 <vlad_semyon@bk.ru>
2026-03-05 17:10:28 +0700

    New Functions

--- app/src/main/java/com/example/izbushka/MainActivity.kt
+++ app/src/main/java/com/example/izbushka/MainActivity.kt
@@ -10,17 +10,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            val intent = Intent(this, InfoMenu::class.java)
+        // Находим кнопку по ID
+        val btnInfo = findViewById<Button>(R.id.button)
+
+        // Устанавливаем обработчик клика
+        btnInfo.setOnClickListener {
+            // Создаем Intent для перехода на SecondActivity
+            val intent = Intent(this, InfoActivity::class.java)
+            startActivity(intent) // Запускаем новую Activity
+        }
+        val btnFunc = findViewById<Button>(R.id.button6)
+        btnFunc.setOnClickListener {
+            val intent = Intent(this, FunctionActivity::class.java)
             startActivity(intent)
         }
-    }
-}
-
-class InfoMenu : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.info_menu)
     }
 }
