84c807bcb6790cc3d38abd30c68d395f3c1efb2e
Novi4ek2156 <vlad_semyon@bk.ru>
2026-03-12 17:49:45 +0700

    Changes

--- app/src/main/java/com/example/izbushka/MainActivity.kt
+++ app/src/main/java/com/example/izbushka/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.izbushka
 
-import android.os.Bundle
 import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.zeromq.SocketType
+import org.zeromq.ZMQ
+import org.zeromq.ZContext
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +32,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, FunctionActivity::class.java)
             startActivity(intent)
         }
+        val btnConnect = findViewById<Button>(R.id.bt_connect)
+        btnConnect.setOnClickListener{
+            val intent = Intent(this, ConnectActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
