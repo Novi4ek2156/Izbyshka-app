802f7be8b8651a47adf80773537fbcbc9b19037b
Metlaev <jojotopnikita@gmail.com>
2026-03-26 20:19:05 +0700

    added changes to frontend

--- app/src/main/java/com/example/izbushka/MainActivity.kt
+++ app/src/main/java/com/example/izbushka/MainActivity.kt
@@ -1,9 +1,22 @@
 package com.example.izbushka
 
+
 import android.os.Bundle
 import android.content.Intent
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.zeromq.SocketType
+import org.zeromq.ZMQ
+import org.zeromq.ZContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Находим кнопку по ID
-        val btnInfo = findViewById<Button>(R.id.button)
+        val btnInfo = findViewById<Button>(R.id.bt_info)
 
         // Устанавливаем обработчик клика
         btnInfo.setOnClickListener {
@@ -19,10 +32,20 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, InfoActivity::class.java)
             startActivity(intent) // Запускаем новую Activity
         }
-        val btnFunc = findViewById<Button>(R.id.button6)
+        val btnFunc = findViewById<Button>(R.id.bt_func)
         btnFunc.setOnClickListener {
             val intent = Intent(this, FunctionActivity::class.java)
             startActivity(intent)
         }
+
+        val btnConnect = findViewById<Button>(R.id.bt_connect)
+        btnConnect.setOnClickListener{
+            val intent = Intent(this, ConnectActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    fun Innactive(view: View) {
+        Toast.makeText(applicationContext, "Временно не работает(", Toast.LENGTH_SHORT).show()
+
     }
 }
